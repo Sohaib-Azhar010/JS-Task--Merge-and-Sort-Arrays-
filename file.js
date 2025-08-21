6420911158060d50a9e5a13a4c0f353bc0461dac
Sohaib Azhar <sohaibazhar04@gmail.com>
2025-08-22 01:27:33 +0500

    did  it using export default function

--- file.js
+++ file.js
@@ -1,36 +1,39 @@
-let listONE = [-3, -1, 9, 10];
-let listTWO = [-10, 3, 4, 6, 9];
+export default function mergeAndSort() {
+    // Take user input
+    let listONE = prompt("Enter elements for List ONE (comma separated):").split(",").map(Number);
+    let listTWO = prompt("Enter elements for List TWO (comma separated):").split(",").map(Number);
 
+    let merged = [];
+    let k = 0;
 
-let merged = []
-let k = 0
+    // Copy elements of listONE
+    for (let i = 0; i < listONE.length; i++) {
+        merged[k] = listONE[i];
+        k++;
+    }
+    console.log("ListOne Elements into temporary array = " + "[" + merged + "]");
 
+    // Copy elements of listTWO
+    for (let i = 0; i < listTWO.length; i++) {
+        merged[k] = listTWO[i];
+        k++;
+    }
+    console.log("ListTwo Elements merged into temporary array = " + "[" + merged + "]");
+    console.log("Now length of merged array is = " + k);
 
-for(let i=0 ; i < listONE.length ; i++){
-    merged[k] = listONE[i]
-    k++;
-}
- 
-console.log("ListOne Elements into temporary array ="+ "["+merged+"]")
-
-for(let i=0 ; i < listTWO.length ; i++){
-    merged[k] = listTWO[i]
-    k++;
-}
-
-console.log("ListTwo Elements merged into temporary array =" + "["+merged+"]")
-console.log("Now length of merged array is = " + k)
-
-//Bubble Sort
-for (let i = 0; i < k - 1; i++) {
-    for (let j = 0; j < k - i - 1; j++) {
-        if (merged[j] > merged[j + 1]) {
-            let temp = merged[j];
-            merged[j] = merged[j + 1];
-            merged[j + 1] = temp;
+    // Bubble Sort
+    for (let i = 0; i < k - 1; i++) {
+        for (let j = 0; j < k - i - 1; j++) {
+            if (merged[j] > merged[j + 1]) {
+                let temp = merged[j];
+                merged[j] = merged[j + 1];
+                merged[j + 1] = temp;
+            }
         }
     }
+
+    console.log("Sorted Array = " + "[" + merged + "]");
 }
 
-
-console.log("Sorted Array="+ "["+merged+"]")
+// Call the function immediately
+mergeAndSort();
